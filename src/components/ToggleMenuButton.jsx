4f18e55d597cf057590b3f0264c97e7c21d1e198
Favio Gabriel Munayco Rivera <75335038+JebGy@users.noreply.github.com>
2025-04-15 22:52:54 +0000

    feat: Menu de opciones

--- src/components/ToggleMenuButton.jsx
+++ src/components/ToggleMenuButton.jsx
@@ -1,5 +1,6 @@
 // src/components/ToggleMenuButton.jsx
 import { useState } from "react";
+import MenuOpciones from "./Menu";
 
 export default function ToggleMenuButton() {
   // Estado para abrir o cerrar el menú
@@ -14,43 +15,17 @@ export default function ToggleMenuButton() {
     <div>
       {/* Botón de menú, solo visible en dispositivos móviles */}
       <button
-        onClick={toggleMenu}
-        className="text-green-900 text-3xl md:hidden" // Visible solo en móviles
-        aria-label="Abrir menú"
+        onClick={() => {
+          console.log("Holaaa")
+          toggleMenu();
+        }}
+        className="text-green-900 text-3xl md:hidden p-4" // Visible solo en móviles
       >
         ☰
       </button>
-
-      {/* Menú deslizable */}
-      <div
-        className={`fixed top-0 left-0 w-64 h-full bg-white border-r border-green-900 transition-transform duration-300 ease-in-out ${
-          isMenuOpen ? "translate-x-0" : "-translate-x-full"
-        }`} // Anima el menú deslizándose desde la izquierda
-      >
-        <nav className="flex flex-col space-y-4 p-6 text-green-900 text-sm font-semibold">
-          <a href="#" className="px-4 py-2 rounded-full bg-amber-100/30">
-            INICIO
-          </a>
-          <a
-            href="#"
-            className="px-4 py-2 rounded-full hover:bg-amber-100 hover:text-green-950 transition-all"
-          >
-            SOBRE NOSOTROS
-          </a>
-          <a
-            href="#"
-            className="px-4 py-2 rounded-full hover:bg-amber-100 hover:text-green-950 transition-all"
-          >
-            TOURS
-          </a>
-          <a
-            href="#"
-            className="px-4 py-2 rounded-full hover:bg-amber-100 hover:text-green-950 transition-all"
-          >
-            CONTACTO
-          </a>
-        </nav>
-      </div>
+      <MenuOpciones isVisible={isMenuOpen} updateState={() => {
+        toggleMenu();
+      }} />
     </div>
   );
 }
